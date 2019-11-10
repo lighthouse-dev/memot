66a1d6a98398228ae491d2f5e1dc3b05a7a7b1b2
lighthouse-dev <lighthouse2.dev@gmail.com>
2019-11-10 15:05:55 +0900

    ログイン画面作成

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight
+} from 'react-native';
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}> ログイン </Text>
+        <TextInput style={styles.input} value="Emaail Address" />
+        <TextInput style={styles.input} value="Password" />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            alert('Login');
+          }}
+        >
+          <Text style={styles.buttonTitle}>Login</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#FFF'
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#EEE',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    padding: 8
+  },
+  button: {
+    backgroundColor: '#FF4181',
+    height: 48,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '70%'
+  },
+  buttonTitle: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+
+export default LoginScreen;
